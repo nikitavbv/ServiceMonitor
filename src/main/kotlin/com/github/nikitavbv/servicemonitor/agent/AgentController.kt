e0f40a367adf5c58706dec603d75693bc3afdb19
Nikita Volobuev <nikitavbv@gmail.com>
2019-01-23 19:28:33 +0200

    refactor(AgentController): move endpoint schedule to EndpointController

--- src/main/kotlin/com/github/nikitavbv/servicemonitor/agent/AgentController.kt
+++ src/main/kotlin/com/github/nikitavbv/servicemonitor/agent/AgentController.kt
@@ -19,11 +19,6 @@ import com.github.nikitavbv.servicemonitor.metric.resources.UptimeMetricReposito
 import com.github.nikitavbv.servicemonitor.project.ProjectNotFoundException
 import com.github.nikitavbv.servicemonitor.project.ProjectRepository
 import com.github.nikitavbv.servicemonitor.user.ApplicationUserRepository
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.CloseableHttpClient
-import org.apache.http.impl.client.HttpClients
-import org.springframework.http.HttpStatus
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -32,7 +27,6 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.lang.Exception
 import java.lang.RuntimeException
 import javax.servlet.http.HttpServletRequest
 
@@ -53,60 +47,6 @@ class AgentController(
     val nginxMetricRepository: NginxMetricRepository,
     val mysqlMetricRepository: MysqlMetricRepository
 ) {
-
-    @Scheduled(fixedRate = 1000 * 60)
-    fun checkEndpoints() {
-        agentRepository.findAll()
-            .filter {
-                it.type == "endpoint"
-            }
-            .forEach {
-                val endpoint = it.properties["endpoint"]
-
-                if (!it.properties.containsKey("totalRequests")) {
-                    it.properties["totalRequests"] = "0"
-                }
-                if (!it.properties.containsKey("totalTime")) {
-                    it.properties["totalTime"] = "0"
-                }
-                if (!it.properties.containsKey("totalErrors")) {
-                    it.properties["totalErrors"] = "0"
-                }
-
-                val httpClient: CloseableHttpClient? = HttpClients.createMinimal()
-                val httpGet = HttpGet(endpoint)
-                val startedAt = System.currentTimeMillis()
-                var totalTime: Long
-                try {
-                    val response = httpClient?.execute(httpGet)
-                    totalTime = System.currentTimeMillis() - startedAt
-                    if (response?.statusLine?.statusCode != HttpStatus.OK.value()) {
-                        it.properties["totalErrors"] = ((it.properties["totalErrors"])!!.toLong() + 1).toString()
-                    }
-                    response?.close()
-                } catch (e: Exception) {
-                    totalTime = System.currentTimeMillis() - startedAt
-                    it.properties["totalErrors"] = ((it.properties["totalErrors"])!!.toLong() + 1).toString()
-                }
-                it.properties["totalTime"] = ((it.properties["totalTime"])!!.toLong() + totalTime).toString()
-                it.properties["totalRequests"] = ((it.properties["totalRequests"])!!.toLong() + 1).toString()
-                httpClient?.close()
-
-                agentRepository.save(it)
-            }
-    }
-
-    @Scheduled(fixedRate = 1000 * 60 * 60 * 24)
-    fun resetEndpointStats() {
-        agentRepository.findAll()
-            .filter { it.type == "endpoint" }
-            .forEach {
-                it.properties["totalRequests"] = "0"
-                it.properties["totalTime"] = "0"
-                it.properties["totalErrors"] = "0"
-                agentRepository.save(it)
-            }
-    }
 
     @GetMapping()
     fun getAgentDetails(httpRequest: HttpServletRequest, token: String): Map<String, Any?> {
